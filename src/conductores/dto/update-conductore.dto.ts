ba4456718f09ec95cdf1a2e64ecb25711d5dc61a
sparra2004 <sparra2004@javerianacali.edu.co>
2026-05-12 15:15:36 -0500

    se creo nueva carpeta

--- src/conductores/dto/update-conductore.dto.ts
+++
@@ -1,5 +0,0 @@
-import { PartialType } from '@nestjs/mapped-types';
-import { CreateConductorDto } from './create-conductore.dto';
-
-
-export class UpdateConductoreDto extends PartialType(CreateConductorDto) {}
--- src/viajes/dto/update-viaje.dto.ts
+++
@@ -1,4 +0,0 @@
-import { PartialType } from '@nestjs/mapped-types';
-import { CreateViajeDto } from './create-viaje.dto';
-
-export class UpdateViajeDto extends PartialType(CreateViajeDto) {}
